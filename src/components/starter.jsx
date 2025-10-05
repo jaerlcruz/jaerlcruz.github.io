3427c11a1926b9b0c45d80f4de804e4acace2ca2
Justine Cruz <jaeraelcruz@gmail.com>
2025-10-05 12:42:01 -0700

    Dark mode adjustment

--- src/components/starter.jsx
+++ src/components/starter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Starter({title, price, Icon, link}) {
     return (
         <li className="flex justify-between py-1 text-sm md:text-base">
-            <a className={(link?`hover:text-[#5A310D] hover:cursor-pointer hover:underline `:``)+`flex items-center gap-2 transition-all ease-in-out`} href={link} target="_blank">
+            <a className={(link?`hover:text-[#5A310D] hover:cursor-pointer hover:underline dark:hover:text-[#BF963C]`:``)+` flex items-center gap-2 transition-all ease-in-out`} href={link} target="_blank">
                 {Icon &&
                 <Icon className=""/>}
                 <p className="font-bold">{title}</p>
